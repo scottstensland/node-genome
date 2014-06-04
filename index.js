1a5a41fbcba6412e0c2fac0f2fb22876b3bcf974
Scott Stensland <scottstensland@gmail.com>
2014-06-04 17:49:16 -0400

    modified:   index.js

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
 
-module.exports = {
-
-	genome : function (spec, my) {
-		
-		var that = {};
-
-		my = my || {};
-
-		var get_name = function () {
-
-			return spec.name;
-		};
-		that.get_name = get_name;
-
-		var says = function () {
-
-			return spec.saying || "Wiers";
-		};
-		that.says = says;
-
-		return that;
-	},
+var genome = function (spec, my) {
 	
-	hello_corinde : function() {
+	var that = {};
 
-		return "Corinde";
-	}
+	my = my || {};
+
+	var get_name = function () {
+
+		return spec.name;
+	};
+	that.get_name = get_name;
+
+	var says = function () {
+
+		return spec.saying || "Wiers";
+	};
+	that.says = says;
+
+	return that;
 };
 
