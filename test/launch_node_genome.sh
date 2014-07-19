624748e3e7dfa20b9dfab7840e4a1a46e22b866b
Scott Stensland <scottstensland@gmail.com>
2014-07-18 22:26:30 -0400

    modified:   test/launch_node_genome.sh

--- test/launch_node_genome.sh
+++ test/launch_node_genome.sh
@@ -1,7 +1,7 @@
 
 
-node ./node_cli.js   # this is normal mode - NON tinkering
+# node ./node_cli.js   # this is normal mode - NON tinkering
   
-# node ./node_cli.js nubia    # tinkering mode
+node ./node_cli.js nubia    # tinkering mode
 
 
